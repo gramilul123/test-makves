e79c11677b9aa1cd6bed3ce3a36a736eb3acb7ec
Гилаев Рамиль <gramil.ul@gmail.com>
2023-01-12 23:45:09 +0400

    Add logic level

---
+++ internal/usecases/usecases.go
@@ -0,0 +1,50 @@
+package usecases
+
+import (
+	"context"
+
+	"github.com/gramilul123/test-makves/internal/models"
+	"github.com/gramilul123/test-makves/internal/repositories"
+)
+
+type Service interface {
+	Set(ctx context.Context, url string) error
+	Get(ctx context.Context, in []string) ([]*models.User, error)
+}
+
+type ServiceUC struct {
+	redis  repositories.Redis
+	makves repositories.Makves
+}
+
+func NewServiceUC(
+	redis repositories.Redis,
+	makves repositories.Makves,
+) *ServiceUC {
+
+	return &ServiceUC{
+		redis:  redis,
+		makves: makves,
+	}
+}
+
+func (f *ServiceUC) Set(ctx context.Context, url string) error {
+
+	usersList, err := f.makves.Download(ctx, url)
+	if err != nil {
+
+		return err
+	}
+
+	usersMap := make(map[string]*models.User, len(usersList))
+	for _, user := range usersList {
+		usersMap[user.Id] = user
+	}
+
+	return f.redis.Set(ctx, usersMap)
+}
+
+func (f *ServiceUC) Get(ctx context.Context, in []string) ([]*models.User, error) {
+
+	return f.redis.Get(ctx, in)
+}
